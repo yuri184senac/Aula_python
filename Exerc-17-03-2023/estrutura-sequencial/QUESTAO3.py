41d11ff2c9f52572f73a3326b6ffde9073577126
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2023-03-17 21:45:07 -0300

    trabahlo triangulo

--- Exerc-17-03-2023/estrutura-sequencial/QUESTAO3.py
+++ Exerc-17-03-2023/estrutura-sequencial/QUESTAO3.py
@@ -1,4 +1,17 @@
+import math
 #ENTRADA:
-print('PROGAMA QUE CALCULA A ÁREA E HIPOTENUSA DE UM TRIÂNGULO:');
+print('PROGAMA QUE CALCULA A ÁREA E HIPOTENUSA DE UM TRIÂNGULO:')
 
-cateto_a = float(input('Informe o valor do cate'));
+a = float(input('Informe o valor do cateto a'))
+b = float(input('Infome o valor do cateto b'))
+
+
+area = (a * b)/2
+
+raiz = a**2 + b**2
+
+c = math.sqrt(raiz)
+
+print('O valor da hipotenusa é igual a %.2f' %(c))
+
+
