629c494441bea951ae110829a9274eae25d8c50a
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2023-03-20 21:19:32 -0300

    conversao-decimal-binario

---
+++ binario/conversor-decimal-binario.py
@@ -0,0 +1,23 @@
+#dividendo
+D = 10
+#divisor
+d = 2
+#quociente
+Q = 0
+#resto
+r = 0
+#vetor de binário
+vetorBinario = []
+#Inicio
+i = 0
+while (i < 5):
+    q = D//d #resultado da divisao
+    r = D%d #resto da divsao
+    vetorBinario[0].insert()
+    D = q #proxima divisao
+    i=+1
+    print('hello world')
+    print('vetorBinario -> '+vetorBinario[i])
+
+
+
