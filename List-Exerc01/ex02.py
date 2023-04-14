aa10614da80f0d267d04e189370e523e588a4d78
yuri1709 <yuriconder184@gmail.com>
2023-04-14 16:27:39 -0300

    Ex02

---
+++ List-Exerc01/ex02.py
@@ -0,0 +1,9 @@
+#Escreva um programa que imprima os 100 primeiros números ímpares
+numero = 0
+while numero <= 100:
+    if (numero % 2 != 0):
+        print (numero);
+        numero = numero + 1
+    else:
+       numero = numero + 1
+    
