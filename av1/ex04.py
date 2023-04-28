687aab9b596d11756d94d874e6506c305f856dc7
yuri1709 <yuriconder184@gmail.com>
2023-04-28 04:08:03 -0300

    Exercicio 04

---
+++ av1/ex04.py
@@ -0,0 +1,16 @@
+print('PROGRAMA QUE CALCULA A MÉDIA ARITIMÉTICA DE IDADES DO GRUPO');
+print('1.Insira a quantaidade de idades que quiser');
+print('2.Insira um valor negativo para calcular a média aritimética das idades');
+print('OBS: Enquanto você não inserir um valor negativo, o programa continua pedindo para inserir idades de outras pessoas !');
+
+idade = 0;
+somaIdades = 0;
+contador = 0;
+while (idade >= 0):
+    idade = int(input(f'Insira a idade da pessoa {contador} :'));
+    if (idade > 0):
+        somaIdades = idade + somaIdades;
+        contador = contador + 1;
+#SAÍDA
+print(somaIdades/contador);
+
