3188bf89a66e46e085433b3d51444eddff227abb
yuri1709 <yuriconder184@gmail.com>
2023-04-28 03:45:28 -0300

    Exercicio 03

---
+++ av1/ex03.py
@@ -0,0 +1,17 @@
+print('PROGRAMA QUE LÊ UM NÚMERO INTEIRO QUALQUER MENOR QUE 1000 E APRESENTE TODOS OS N ÍMPARES DE 1 ATÉ N');
+
+#ENTRADA
+numero = int(input('Insira um número qualquer menor do que 1000: '));
+
+#VERIFICAÇÃO
+while numero > 1000:
+    numero = int(input('Insira um número qualquer menor do que 1000: '));
+
+#SAÍDA
+print('');
+print('Listagem de todos os números ímpares menores que',numero);
+print('----------------------------------------------------------')
+for i in range(1, numero):
+    if(i % 2):
+        print(i);
+
