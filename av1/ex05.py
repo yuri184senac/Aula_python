3539667afb439a258849b62747a983b2febd234d
yuri1709 <yuriconder184@gmail.com>
2023-04-28 04:23:59 -0300

    Exercicio 06

---
+++ av1/ex05.py
@@ -0,0 +1,11 @@
+print('PROGRAMA QUE CALCULA E APRESENTA TODOS OS DIVISORES DO NÚMERO DA SUA ESCOLHA');
+
+#ENTRADA
+numero = int(input('Insira por favor o número desejado: '));
+divisor = numero;
+#PROCESSO E SAÍDA DE DADOS
+print('Lista de todos os divisores do número', numero, ':')
+while divisor > 0:
+    if(numero % divisor == 0):
+        print(f'{numero/divisor: .0f}');
+    divisor = divisor - 1;
