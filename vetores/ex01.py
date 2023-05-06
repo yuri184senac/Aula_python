5cad87ba0beee98181567f4fa8a23efe4bb63187
yuri1709 <yuriconder184@gmail.com>
2023-05-06 01:02:24 -0300

    exercicio vetor1

--- vetores/ex01.py
+++ vetores/ex01.py
@@ -1,11 +1,7 @@
-vetor = [23,30,55,3,5,0,1,4,2]
-
-maior = vetor[0];
-menor = vetor[0];
-print(vetor)
+vetor = [23,30,55,3,5,0,1,4,2,100,99,37]
 i = 1;
 x = 0;
-
+print(vetor)
 for i in range(len(vetor)):
     for x in range(len(vetor)):
         if vetor[x] > vetor[i]:
