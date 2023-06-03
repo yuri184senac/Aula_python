a899d42779b841c4150f6fe7d9e2fbb636f5fcb3
yuri1709 <yuriconder184@gmail.com>
2023-06-03 03:04:29 -0300

    Desafio 1

--- criptografia/criptografia.py
+++ criptografia/criptografia.py
@@ -1,16 +1,16 @@
-lista = [
-    ["z" , "p"],
-    ["e" , "o"],
-    ["n" , "l"],
-    ["i" , "a"],
-    ["t" , "r"],
-    
-    ["p" , "z"],
-    ["o" , "e"],
-    ["l" , "n"],
-    ["a" , "i"],
-    ["r" , "t"]
-]
+lista = {
+        "z" : "p", 
+        "e" : "o" ,
+        "n" : "l",
+        "i" : "a" ,
+        "t" : "r" ,
+        
+        "p" : "z" ,
+        "o" : "e" ,
+        "l" : "n" ,
+        "a" : "i" ,
+        "r" : "t"
+}
 
 letras = {
     'á':'a',
@@ -29,20 +29,21 @@ def sanitizarEntradaDeDados(frase):
         for letraEspecial, letraNormal in letras.items():
             if char[1] == letraEspecial:
               frase = frase.replace(letraEspecial, letraNormal)      
-  
 
                   
 def parseToZeus(frase):
-    try:
-        for char in enumerate(frase):
-            for letraAntiga, letraNova in lista.items():
-                print(letraNova)
-                if char[1] == letraAntiga :
-                    # print(char[1], '<-->', letraNova)           
-                    frase = frase.replace(char[1], letraNova)
+    # try:
+        listaFrase = list(frase);
+        for char in enumerate(listaFrase):
+           for charAntigo, charNovo in lista.items():
+               if char[1] == charAntigo:
+                listaFrase[char[0]] = charNovo;
+                frase = ''.join(listaFrase)
         print(frase)
-    except:
-        print('Ocorreu um erro no código durante a execução do programa2')
+                            
+       
+    # except:
+    #     print('Ocorreu um erro no código durante a execução do programa2')
 
 
 parseToZeus('todo mundo age nao apenas por compulsao externa, mas tambem por necessidade intima') 
