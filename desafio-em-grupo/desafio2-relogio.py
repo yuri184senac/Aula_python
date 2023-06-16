b806f4057a707db73bc0613a23d466b127511255
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2023-06-16 20:51:24 -0300

    k

--- desafio-em-grupo/desafio2-relogio.py
+++ desafio-em-grupo/desafio2-relogio.py
@@ -16,18 +16,23 @@ numero = []
 #CHAR[1]= SIMBOLO ROMANO
 #NUM[0] = SIMBOLO ROMANO
 def parseToDecimal(algRomano):
-
+    subtrair = 0
     soma = 0
     for char in enumerate(algRomano):    
         for num in lista.items():
             if(num[0] == char[1]):
                numero.append(num[1])
 
-    for i in numero:
-        soma = i + soma
-    print(soma)
+    for i in range(len(numero)):
+        if numero[i] < numero[i+1]:
+            subtrair =  i - subtrair
+        else:   
+            soma = i + soma
+    
+            
+    print(subtrair)
     print(numero)
-
+    
 parseToDecimal(algRomano) 
 
 
