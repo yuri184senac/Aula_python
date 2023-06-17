df8dbddcee8db69ce9cb2c5c536c463b3c65d473
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2023-06-16 21:46:19 -0300

    terminar dirversosloops2

---
+++ bonus/diversosLoops2.py
@@ -0,0 +1,19 @@
+#Participantes:
+# João Saad
+# Thiago Braga
+# Guilherme Lopes
+# Yuri Roliz
+
+
+
+while True:
+    qtde = int(input('Insira um valor n'))
+    
+    if qtde < 0: break
+    else {
+        print('Insira um número maior do que 0');
+    }
+    
+    
+
+    
