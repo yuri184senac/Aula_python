909433325185b494dae86f5af3e11c93d5cd60f8
yuri1709 <yuriconder184@gmail.com>
2023-06-23 18:01:49 -0300

    nao copiar esse codigo

--- av2/senhaCriticada.py
+++ av2/senhaCriticada.py
@@ -6,6 +6,8 @@ def verLetraMaiu(senha):
     if senha.lower() == senha:
         print('sua senha não tem letra Maiúscula')
         return False 
+    else:
+        return True
       
 
 def verTamanho(senha):
@@ -15,18 +17,25 @@ def verTamanho(senha):
         return False
 
 def verSeComecaNumero(senha):
-    if senha[0].isDigit():
+    if senha[0].isdigit():
         print('primeiro item é um numero')
         return False
+    else:
+        return True
 
-def verNumero(senha):
+def verNumero(senha): 
     for i in senha:
-        if senha[i].isDigit():
+        if i.isdigit():
             return True
         else:
             print('erro: A senha não tem números')
             return False
 
 
-def validarSenha(senha):
-    
+
+# def validarSenha(senha):
+#     if (verTamanho(senha) and verLetraMaiu(senha) and verSeComecaNumero(senha) and verNumero(senha)):
+#         print('SENHA VÁLIDA')
+#     else:
+#         print('SENHA INVÁLIDA')
+
