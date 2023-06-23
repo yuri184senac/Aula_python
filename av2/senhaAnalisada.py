2fca6b77193d1f981f000ac2d91705a32a92814e
yuri1709 <yuriconder184@gmail.com>
2023-06-23 18:27:26 -0300

    senhaAnalisada

---
+++ av2/senhaAnalisada.py
@@ -0,0 +1,11 @@
+#GRUPO
+#Guilherme
+#João Gabriel
+#Thiago Luiz
+#Yuri Roliz
+senha = input('Informe a senha: ');
+
+print(f'Senha normal: {senha}')
+print(f'Tamanho da senha: {len(senha)}')
+print(f'Versão maiúscula da senha  {senha.upper()}')
+print(f'Versão minúscula da senha {senha.lower()}')
