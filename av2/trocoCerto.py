082696172bdadb0f69c1a96623b18977a4b4775e
yuri1709 <yuriconder184@gmail.com>
2023-06-23 18:50:16 -0300

    Troco Certo - terminado

--- av2/trocoCerto.py
+++ av2/trocoCerto.py
@@ -8,23 +8,14 @@ print('MERCADO');
 produto_valor= float(input('Infome o valor do produto R$:'))
 valor_pago = float(input('Informe o valor pago em dinheiro R$:'))
 troco = valor_pago - produto_valor
-meuTroco = 'Seu troco é' 
+meuTroco = 'No troco você vai precisar de \n  ' 
 
 #processo
 def separaCedulas(valor):
   global meuTroco 
 
-  n200 = valor // 200
-  resto = valor % 200
-
-  n100 = resto // 100
-  resto1 = resto % 100
-
-  n50 = resto1 // 50
-  resto2 = resto1 % 50
-
-  n20 = resto2 // 20
-  resto3 = resto2 % 20
+  n20 = valor // 20
+  resto3 = valor % 20
 
   n10 = resto3 // 10
   resto4 = resto3 % 10
@@ -33,31 +24,23 @@ def separaCedulas(valor):
   resto5 = resto4  % 5
 
   n2 = resto5 // 2
-  n1 = resto5 // 1
+  n1 = resto5 % 2
 
-  if (n200 != 0):
-    meuTroco += f' {n200:.0f} x notas de 200'
-
-  if (n100 != 0):
-    meuTroco += f' {n100:.0f} x notas de 100'
-
-  if (n50 != 0):
-    meuTroco += f' {n50:.0f} x notas de 50'
 
   if (n20 != 0):
-    meuTroco += f' {n20:.0f} x notas de 20'
+    meuTroco += f' {n20:.0f} x notas de 20 \n '
 
   if (n10 != 0):
-    meuTroco += f' {n10:.0f} x notas de 10'
+    meuTroco += f' {n10:.0f} x notas de 10 \n '
 
   if (n5 != 0):
-    meuTroco += f' {n5:.0f} x notas de 5'
+    meuTroco += f' {n5:.0f} x notas de 5 \n '
   
   if (n2 != 0):
-    meuTroco += f' {n2:.0f} x notas de 100'
+    meuTroco += f' {n2:.0f} x notas de 2 \n '
   
   if (n1 != 0):
-    meuTroco += f' {n1:.0f} x notas de 1'
+    meuTroco += f' {n1:.0f} x notas de 1 \n '
     
 
 separaCedulas(troco)
